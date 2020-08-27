9d394bf2be33735edcc69334a993364f42644361
WizardFromBlizzard <tekilaaliket@hotmail.com>
2020-08-27 16:23:42 +0200

    category page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TodoComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
