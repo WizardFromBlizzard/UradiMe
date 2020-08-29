68219dc189928984fe1ace7a4b5e533716f6210e
WizardFromBlizzard <tekilaaliket@hotmail.com>
2020-08-29 22:33:45 +0200

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDVhG23tT-YkRKZqmqFaZAS034y1I0xrC4",
+    authDomain: "uradime-26fb6.firebaseapp.com",
+    databaseURL: "https://uradime-26fb6.firebaseio.com",
+    projectId: "uradime-26fb6",
+    storageBucket: "uradime-26fb6.appspot.com",
+    messagingSenderId: "318108259240",
+    appId: "1:318108259240:web:99e5ece3893153a64a439d",
+    measurementId: "G-KDLZPBYB61"
+  }
 };
